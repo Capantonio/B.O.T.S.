9dc49cfab5576398c7f693a7ee3c908396ae1506
Fabio Capitanio <fabiocap95@live.it>
2018-05-27 17:04:36 +0200

    First Commit

---
+++ src/bots/controller/view/LoginClass.java
@@ -0,0 +1,17 @@
+package bots.controller.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginClass {
+
+	@FXML
+    private void handleOk() {
+		
+		
+	}
+	
+}
